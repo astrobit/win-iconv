30a710b858113989639d25bc3e90fdd7aee506ea
Brian W. Mulligan <astrobit2@yahoo.com>
2018-02-20 12:22:11 -0600

    move folders around and create 32 bit and 64 bit versions of the DLL and installers; set DLL compilation flags

--- iconv.h
+++ iconv.h
@@ -1,6 +1,14 @@
 #ifndef _LIBICONV_H
 #define _LIBICONV_H
 #include <stddef.h>
+#if defined _WIN32 || _WIN64
+# ifdef WINICONV_EXPORTS
+#define DLLSPEC __declspec(dllexport)
+# else
+#define DLLSPEC __declspec(dllimport)
+# endif
+#endif
+
 #ifndef WINICONV_CONST
 # ifdef ICONV_CONST
 #  define WINICONV_CONST ICONV_CONST
@@ -12,10 +20,13 @@
 extern "C" {
 #endif
 typedef void* iconv_t;
-iconv_t iconv_open(const char *tocode, const char *fromcode);
-int iconv_close(iconv_t cd);
-size_t iconv(iconv_t cd, WINICONV_CONST char **inbuf, size_t *inbytesleft, char **outbuf, size_t *outbytesleft);
+DLLSPEC iconv_t iconv_open(const char *tocode, const char *fromcode);
+DLLSPEC int iconv_close(iconv_t cd);
+DLLSPEC size_t iconv(iconv_t cd, WINICONV_CONST char **inbuf, size_t *inbytesleft, char **outbuf, size_t *outbytesleft);
 #ifdef __cplusplus
 }
 #endif
+#endif
+#ifdef WIN32
+#undef DLLSPEC
 #endif
--- win_iconv.c
+++ win_iconv.c
@@ -49,11 +49,8 @@ typedef unsigned char uchar;
 typedef unsigned short ushort;
 typedef unsigned int uint;
 
-typedef void* iconv_t;
-
-iconv_t iconv_open(const char *tocode, const char *fromcode);
+#include <iconv.h>
 int iconv_close(iconv_t cd);
-size_t iconv(iconv_t cd, const char **inbuf, size_t *inbytesleft, char **outbuf, size_t *outbytesleft);
 
 /* libiconv interface for vim */
 #if defined(MAKE_DLL)
